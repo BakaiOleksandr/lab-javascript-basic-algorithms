1ffb122ac34e7115c90d13bc9bfd4241bf4d2943
Oleksandr Bakai <saniabakay@gmail.com>
2025-06-14 15:40:59 +0200

    NameLoops

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-// Iteration 1: Names and Input
+
+let hacker1 = prompt("Enter your name hacker1"); //The driver's name is XXXX
+let space = "";
+for(let i = hacker1.length -1; i>=0; i--){//reverse name with spaces
+    space +=hacker1[i].toUpperCase()+" ";
+}
+alert(space.trim());
 
 
-// Iteration 2: Conditionals
+let hacker2 = prompt("Enter your name hacker2"); //The navigator's name is YYYY
+let space2 = "";
+for(let i = hacker2.length -1; i>=0; i--){//reverse name with spaces
+    space2 +=hacker2[i].toUpperCase()+" ";
+}
+alert(space2.trim());
+
+//Longest name
+if(hacker1.length > hacker2.length){
+    alert(`Hacker1 has longer name it has ${hacker1.length} characters.`);
+}
+else if (hacker1.length < hacker2.length){
+        alert(`Hacker2 has longer name it has ${hacker2.length} characters.`);
+    }
+else{
+    alert(`Wow, you both have equally long names, ${hacker1.length} characters!`);
+    }
 
 
-// Iteration 3: Loops
+//Alphabeticaly first
+if (hacker1<hacker2){
+    alert(`The ${hacker1.toUpperCase()} alphabeticaly goes first.`);
+}
+  else if (hacker1>hacker2){
+    alert(`The ${hacker2.toUpperCase()} is alphabeticaly goes first.`);
+}
+else{
+    alert("You have the same name!");
+}  
